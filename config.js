7184c7aaa45656509f95402b43ceb20b7ca30191
p1nox <pinox101@gmail.com>
2016-06-24 12:54:48 -0400

    new passports added, config changed; version bumped

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   interval: 60000 * 60,
   selector: '#rightwrapp .moduletable table a',
   element_alt: 'boton pasaportes',
-  last_pdf_href: '/images/Lista_de_pasaportes_actualizada_al_18.05.16.pdf'
+  last_pdf_href: '/images/Lista_de_pasaportes22.06.16.pdf'
 };
